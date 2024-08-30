1d3babf23e28d6a8714e3582efc53241ce873a16
Daniel San <danielgdt88@hotmail.com>
2024-08-29 21:03:01 -0300

    arquivos public

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 function App() {
-  return <div className="bg-blue-500 underline font-bold ">s</div>;
+  return <div className="bg-blue-500 h-screen">s</div>;
 }
 
 export default App;
