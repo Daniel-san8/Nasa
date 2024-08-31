8ee4997ce870472855d7211da6491ccd9f3c9b3c
Daniel San <danielgdt88@hotmail.com>
2024-08-31 20:21:02 -0300

    feat: typography main

--- src/Components/Main.tsx
+++ src/Components/Main.tsx
@@ -2,10 +2,10 @@ import React from "react";
 
 const Main = () => {
   return (
-    <main className="flex flex-col font-bellefair p-12 text-blue-secondary gap-y-12	">
+    <main className="flex flex-col items-center	font-bellefair p-12 text-blue-secondary gap-y-12	">
       <h1>ENTÃO, VOCÊ QUER VIAJAR PARA O</h1>
       <h2 className="text-white ">ESPAÇO</h2>
-      <p>
+      <p className="font-barlow text-center">
         Vamos encarar; se você quiser ir para o espaço, pode realmente ir para o
         espaço sideral e não ficar pairando na borda dele. Então, sente-se e
         relaxe, porque nós lhe daremos uma experiência verdadeiramente fora
