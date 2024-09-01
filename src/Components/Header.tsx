13a840b2dcf1cbf837b804c43b1777a236adefde
Daniel San <danielgdt88@hotmail.com>
2024-09-01 19:51:53 -0300

    feat: md header

--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -2,17 +2,33 @@ import React from "react";
 
 const Header = () => {
   return (
-    <header className=" flex items-center justify-between py-8">
-      <div className="px-8">
+    <header className="flex items-center justify-between">
+      <div className="h-full m-full px-6 py-6 md:px-10">
         <img src="shared/logo.svg" alt="logo" />
       </div>
-      <nav className="px-8">
+      <nav className="px-8 md:hidden">
         <img
           className="cursor-pointer"
           src="shared/icon-hamburger.svg"
           alt="hamburguer"
         />
       </nav>
+      <div className="hidden md:flex pl-24 pr-12 h-24 self-start  bg-white backdrop-filter bg-opacity-5 backdrop-blur-xl lg:hidden">
+        <div className="font-barlow text-white tracking-widest self-center">
+          <ul className="flex">
+            <li>Home</li>
+            <li>
+              <span className="font-bold pr-3 pl-10">01</span>Destination
+            </li>
+            <li>
+              <span className="font-bold pr-3 pl-12">02</span>Crew
+            </li>
+            <li>
+              <span className="font-bold pr-3 pl-12">03</span>Technology
+            </li>
+          </ul>
+        </div>
+      </div>
     </header>
   );
 };
