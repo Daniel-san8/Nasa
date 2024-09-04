d971169f8cf17a2598ef247c16da2d494881d2db
Daniel San <danielgdt88@hotmail.com>
2024-09-03 21:50:02 -0300

    pseudo

--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Header = () => {
   return (
-    <header className="flex items-center justify-between lg:pt-10">
+    <header className="flex items-center justify-between lg:pt-10 ">
       <div className="h-full m-full px-6 py-6 md:px-10">
         <img src="shared/logo.svg" alt="logo" />
       </div>
@@ -13,7 +13,7 @@ const Header = () => {
           alt="hamburguer"
         />
       </nav>
-      <div className="hidden before-content md:flex pl-24 pr-12 h-24 self-start  bg-white backdrop-filter bg-opacity-5 backdrop-blur-xl lg:backdrop-blur-sm ">
+      <div className="hidden  md:flex pl-24 pr-12 h-24 self-start  bg-white backdrop-filter bg-opacity-5 backdrop-blur-xl lg:backdrop-blur-sm lg:before-content">
         <div className="font-barlow text-white tracking-widest self-center lg:pr-16">
           <ul className="flex">
             <li>Home</li>
