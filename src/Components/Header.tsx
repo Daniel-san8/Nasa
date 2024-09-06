f9a80d6ddc81d8fc71d78e9cdf793f1a00b52714
Daniel San <danielgdt88@hotmail.com>
2024-09-05 22:03:28 -0300

    feat: dec

--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import { globalUse } from "../Context/GlobalContext";
 
 const Header = () => {
-  const hamburguer = React.useRef<null | HTMLElement>(null);
   const { menuActive, setMenuActive } = globalUse();
 
   return (
@@ -12,7 +11,6 @@ const Header = () => {
       </div>
       <nav
         className="px-8 md:hidden"
-        ref={hamburguer}
         onClick={() => setMenuActive(!menuActive)}
       >
         <img
@@ -20,31 +18,32 @@ const Header = () => {
           src="shared/icon-hamburger.svg"
           alt="hamburguer"
         />
-        <div className="hidden  md:hidden pl-24 pr-12 h-24 self-start  bg-white backdrop-filter bg-opacity-5 backdrop-blur-xl lg:backdrop-blur-sm lg:before-content ">
-          <div className="font-barlow text-white tracking-widest self-center lg:pr-16">
-            <ul className="flex whitespace-nowrap">
-              <li>
-                <span className="font-bold mr-3 ml-10 after-content cursor-pointer">
-                  00
-                </span>
-                <span className="cursor-pointer">Home</span>
-              </li>
-              <li>
-                <span className="font-bold mr-3 ml-10 cursor-pointer">01</span>
-                <span className="cursor-pointer">Destination</span>
-              </li>
-              <li>
-                <span className="font-bold mr-3 ml-12 cursor-pointer">02</span>
-                <span className="cursor-pointer">Crew</span>
-              </li>
-              <li>
-                <span className="font-bold mr-3 ml-12 cursor-pointer">03</span>
-                <span className="cursor-pointer">Technology</span>
-              </li>
-            </ul>
-          </div>
-        </div>
       </nav>
+
+      <div className="hidden md:flex pl-24 pr-12 h-24 self-start  bg-white backdrop-filter bg-opacity-5 backdrop-blur-xl lg:backdrop-blur-sm lg:before-content ">
+        <div className="font-barlow text-white tracking-widest self-center lg:pr-16">
+          <ul className="flex whitespace-nowrap">
+            <li>
+              <span className="font-bold mr-3 ml-10 after-content cursor-pointer">
+                00
+              </span>
+              <span className="cursor-pointer">Home</span>
+            </li>
+            <li>
+              <span className="font-bold mr-3 ml-10 cursor-pointer">01</span>
+              <span className="cursor-pointer">Destination</span>
+            </li>
+            <li>
+              <span className="font-bold mr-3 ml-12 cursor-pointer">02</span>
+              <span className="cursor-pointer">Crew</span>
+            </li>
+            <li>
+              <span className="font-bold mr-3 ml-12 cursor-pointer">03</span>
+              <span className="cursor-pointer">Technology</span>
+            </li>
+          </ul>
+        </div>
+      </div>
     </header>
   );
 };
