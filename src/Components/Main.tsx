04eb9b46d0e27d653c5904f6f6a1058add71be5c
Daniel San <danielgdt88@hotmail.com>
2024-09-06 22:12:30 -0300

    feat: navigate no button

--- src/Components/Main.tsx
+++ src/Components/Main.tsx
@@ -1,6 +1,9 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
 const Home = () => {
+  const navigate = useNavigate();
+
   return (
     <main className="font-bellefair p-6 text-blue-secondary md:px-20 md:py-32 lg:flex lg:px-40 lg:mt-72 lg:gap-x-8 ">
       <div className="flex flex-col items-center justify-center gap-y-12 xl:items-start">
@@ -16,7 +19,10 @@ const Home = () => {
         </p>
       </div>
       <div className="flex justify-center px-20 py-24 md:py-16 md:px-28  xl:ml-72">
-        <button className="border-8 border-black border-opacity-5 text-blue-primary text-2xl bg-white cursor-pointer px-10 py-14 rounded-full md:px-16 md:py-24 md:text-3xl ">
+        <button
+          onClick={() => navigate("/destination")}
+          className="border-8 border-black border-opacity-5 text-blue-primary text-2xl bg-white cursor-pointer px-10 py-14 rounded-full md:px-16 md:py-24 md:text-3xl "
+        >
           Explorar
         </button>
       </div>
