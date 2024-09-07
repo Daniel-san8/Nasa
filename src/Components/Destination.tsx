3efb1e59919232c88843cd8c8a4a0b6e7210873c
Daniel San <danielgdt88@hotmail.com>
2024-09-07 20:23:44 -0300

    feat: titulo do destination

--- src/Components/Destination.tsx
+++ src/Components/Destination.tsx
@@ -1,7 +1,14 @@
 import React from "react";
 
 const Destination = () => {
-  return <h1>Destination</h1>;
+  return (
+    <div className="h-screen py-12 px-40">
+      <h1 className="font-barlow text-2xl text-white tracking-wide">
+        <span className="font-bold opacity-25 pr-6">01</span>Escolha o seu
+        destino
+      </h1>
+    </div>
+  );
 };
 
 export default Destination;
