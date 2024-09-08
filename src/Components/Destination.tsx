cd5c807b9a300fda5c16cb2f292bd844ce42db5f
Daniel San <danielgdt88@hotmail.com>
2024-09-08 19:01:45 -0300

    wix: parte 1 destination

--- src/Components/Destination.tsx
+++ src/Components/Destination.tsx
@@ -2,11 +2,36 @@ import React from "react";
 
 const Destination = () => {
   return (
-    <div className="h-screen py-12 px-40">
-      <h1 className="font-barlow text-2xl text-white tracking-wide">
+    <div className="h-full flex flex-col items-center">
+      <h1 className="font-barlow text-white tracking-wide p-6">
         <span className="font-bold opacity-25 pr-6">01</span>Escolha o seu
         destino
       </h1>
+
+      <div className="px-6">
+        <div className="py-12">
+          <img src="destination/image-moon.png" width="150px" height="150px" />
+        </div>
+      </div>
+
+      <div>
+        <ul className="flex text-blue-secondary gap-x-8">
+          <li>Moon</li>
+          <li>Mars</li>
+          <li>Europa</li>
+          <li>Titan</li>
+        </ul>
+      </div>
+
+      <div className="relative py-6 flex flex-col items-center after:content-[' '] after:w-5/6 after:h-0.5 after:absolute after:bottom-0 after:bg-barra-after after:opacity-25">
+        <h1 className="text-6xl text-white pb-5">Moon</h1>
+        <p className="text-blue-secondary text-sm text-center px-6">
+          Veja nosso planeta como você nunca viu antes. Uma viagem relaxante,
+          perfeita para ajudar a recuperar a perspectiva e voltar revigorado.
+          Enquanto estiver lá, conheça um pouco da história visitando os locais
+          de pouso da Luna 2 e da Apollo 11.
+        </p>
+      </div>
     </div>
   );
 };
