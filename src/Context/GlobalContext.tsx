f869863afbc155bd0554d266b792839990d79eb0
Daniel San <danielgdt88@hotmail.com>
2024-09-08 20:46:54 -0300

    feat: axios get API

--- src/Context/GlobalContext.tsx
+++ src/Context/GlobalContext.tsx
@@ -1,3 +1,4 @@
+import axios from "axios";
 import React, { PropsWithChildren } from "react";
 import { useLocation } from "react-router-dom";
 
@@ -5,6 +6,43 @@ interface UIGlobal {
   menuActive: boolean;
   setMenuActive: React.Dispatch<React.SetStateAction<boolean>>;
   pathname: string;
+  data: UIData | null;
+}
+
+type Crew = {
+  bio: string;
+  images: {
+    png: string;
+    webp: string;
+  };
+  name: string;
+  role: string;
+};
+
+type Destinations = {
+  description: string;
+  distance: string;
+  images: {
+    png: string;
+    webp: string;
+  };
+  name: string;
+  travel: string;
+};
+
+type Technology = {
+  description: string;
+  images: {
+    png: string;
+    webp: string;
+  };
+  name: string;
+};
+
+interface UIData {
+  crew: Crew[];
+  destinations: Destinations[];
+  technology: Technology[];
 }
 
 const global = React.createContext<UIGlobal | null>(null);
@@ -28,8 +66,25 @@ export const ProviderGlobal = ({ children }: PropsWithChildren) => {
 
   const { pathname } = useLocation();
 
+  // Função para puxar dados da API estática
+
+  const [data, setData] = React.useState<UIData | null>(null);
+
+  React.useEffect(() => {
+    async function reqApi() {
+      try {
+        const api = axios.get("data.json");
+        const { data } = await api;
+        setData(data);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+    reqApi();
+  }, []);
+
   return (
-    <global.Provider value={{ menuActive, setMenuActive, pathname }}>
+    <global.Provider value={{ menuActive, setMenuActive, pathname, data }}>
       {children}
     </global.Provider>
   );
