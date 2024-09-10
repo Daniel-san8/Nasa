23c4a3fb0104a939936c6bf49b8c73890aa14214
Daniel San <danielgdt88@hotmail.com>
2024-09-10 17:02:27 -0300

    feat: Componente textPlanet

---
+++ src/Components/CompDestination/TextPlanet.tsx
@@ -0,0 +1,37 @@
+import React from "react";
+
+type textPlanet = {
+  description: string;
+  distance: string;
+  name: string;
+  travel: string;
+};
+
+const TextPlanet = ({ textPlanet }: { textPlanet?: textPlanet }) => {
+  if (!textPlanet) return null;
+  return (
+    <>
+      <div className="relative py-6 flex flex-col items-center after:content-[' '] after:w-5/6 after:h-0.5 after:absolute after:bottom-0 after:bg-barra-after after:opacity-25">
+        <h1 className="text-6xl text-white pb-5">{textPlanet.name}</h1>
+        <p className="text-blue-secondary text-sm text-center px-6 leading-7">
+          {textPlanet.description}
+        </p>
+      </div>
+
+      <div className="pt-6">
+        <div className="flex flex-col items-center pb-6 gap-y-3 md:flex-initial">
+          <h2 className="text-blue-secondary text-sm">Med. Distancia</h2>
+          <h3 className="text-white text-3xl">{textPlanet.distance}</h3>
+        </div>
+        <div className="flex flex-col items-center pb-6 gap-y-3">
+          <h2 className="text-blue-secondary text-sm">
+            Tempo de viagem estimado
+          </h2>
+          <h3 className="text-white text-3xl">{textPlanet.travel}</h3>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default TextPlanet;
--- src/Components/Destination.tsx
+++ src/Components/Destination.tsx
@@ -1,6 +1,7 @@
 import React from "react";
-import { Destinations, globalUse } from "../Context/GlobalContext";
+import { globalUse } from "../Context/GlobalContext";
 import ImagePlanet from "./CompDestination/ImagePlanet";
+import TextPlanet from "./CompDestination/TextPlanet";
 
 const Destination = () => {
   const { data } = globalUse();
@@ -13,14 +14,10 @@ const Destination = () => {
 
   const planetObj = destinations.find((dest) => dest.name === planet);
 
-  const planetSelect: Destinations[] = [];
-
   const [image, setImage] = React.useState("");
 
   React.useEffect(() => {
     if (planetObj) {
-      planetSelect.shift();
-      planetSelect.push(planetObj);
       setImage(planetObj.images.png);
     }
   }, [planet]);
@@ -51,28 +48,7 @@ const Destination = () => {
         </ul>
       </div>
 
-      <div className="relative py-6 flex flex-col items-center after:content-[' '] after:w-5/6 after:h-0.5 after:absolute after:bottom-0 after:bg-barra-after after:opacity-25">
-        <h1 className="text-6xl text-white pb-5">Moon</h1>
-        <p className="text-blue-secondary text-sm text-center px-6 leading-7">
-          Veja nosso planeta como você nunca viu antes. Uma viagem relaxante,
-          perfeita para ajudar a recuperar a perspectiva e voltar revigorado.
-          Enquanto estiver lá, conheça um pouco da história visitando os locais
-          de pouso da Luna 2 e da Apollo 11.
-        </p>
-      </div>
-
-      <div className="pt-6">
-        <div className="flex flex-col items-center pb-6 gap-y-3 md:flex-initial">
-          <h2 className="text-blue-secondary text-sm">Med. Distancia</h2>
-          <h3 className="text-white text-3xl">384,400 km</h3>
-        </div>
-        <div className="flex flex-col items-center pb-6 gap-y-3">
-          <h2 className="text-blue-secondary text-sm">
-            Tempo de viagem estimado
-          </h2>
-          <h3 className="text-white text-3xl">3 dias</h3>
-        </div>
-      </div>
+      <TextPlanet textPlanet={planetObj ? planetObj : undefined} />
     </div>
   );
 };
