3cd2b82536820be809a88dcba15a5894f5797fc4
Daniel San <danielgdt88@hotmail.com>
2024-09-14 19:52:25 -0300

    feat: dou funcionalidades ao botões para deixar a página dinâmica

--- src/Components/Technology.tsx
+++ src/Components/Technology.tsx
@@ -3,9 +3,10 @@ import { globalUse } from "../Context/GlobalContext";
 
 const Technology = () => {
   const { data } = globalUse();
-  if (!data) return null;
-  const { technology } = data;
 
+  const [indice, setIndice] = React.useState(0);
+
+  if (!data) return null;
   return (
     <div className="flex flex-col items-center h-full">
       <h1 className="text-white tracking-wide pt-6 pb-20">
@@ -14,26 +15,40 @@ const Technology = () => {
       </h1>
 
       <div className="w-full">
-        <img className="h-72 w-full" src={technology[0].images.portrait} />
+        <img
+          className="h-72 w-screen"
+          src={data.technology[indice]?.images.portrait}
+        />
       </div>
 
       <ul className="flex gap-x-4 pt-8 pb-10">
-        <li className="bg-white w-10 h-10 flex justify-center items-center rounded-3xl">
+        <li
+          className="bg-white w-10 h-10 flex justify-center items-center rounded-3xl cursor-pointer"
+          onClick={() => setIndice(0)}
+        >
           1
         </li>
-        <li className="flex justify-center items-center w-10 h-10 border border-white border-opacity-25 rounded-3xl text-white">
+        <li
+          className="flex justify-center items-center w-10 h-10 border border-white border-opacity-25 rounded-3xl text-white cursor-pointer"
+          onClick={() => setIndice(1)}
+        >
           2
         </li>
-        <li className="flex justify-center items-center w-10 h-10 border border-white border-opacity-25 rounded-3xl text-white">
+        <li
+          className="flex justify-center items-center w-10 h-10 border border-white border-opacity-25 rounded-3xl text-white cursor-pointer"
+          onClick={() => setIndice(2)}
+        >
           3
         </li>
       </ul>
 
       <div className="flex flex-col items-center ">
         <h2 className="text-2xl text-blue-secondary  ">A terminologia</h2>
-        <h3 className="text-4xl text-white ">{technology[0].name}</h3>
+        <h3 className="text-4xl text-white ">
+          {data.technology[indice]?.name}
+        </h3>
         <p className="text-center text-blue-secondary leading-6 pt-4 px-6 pb-12">
-          {technology[0].description}
+          {data.technology[indice]?.description}
         </p>
       </div>
     </div>
