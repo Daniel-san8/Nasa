a7566f50d6a77c4e2bbd1326c14f75bc7aebc3b1
Daniel San <danielgdt88@hotmail.com>
2024-09-14 20:51:55 -0300

    responsivo lg technology

--- src/Components/Technology.tsx
+++ src/Components/Technology.tsx
@@ -9,9 +9,9 @@ const Technology = () => {
   const elementoUl = React.useRef<HTMLUListElement>(null);
 
   const estadoActive =
-    "bg-white w-10 h-10 flex justify-center items-center rounded-3xl cursor-pointer";
+    "bg-white w-10 h-10 flex justify-center items-center rounded-3xl cursor-pointer lg:w-20 lg:h-20 lg:rounded-full lg:text-lg";
   const estadoOff =
-    "flex justify-center items-center w-10 h-10 border border-white border-opacity-25 rounded-3xl text-white cursor-pointer";
+    "flex justify-center items-center w-10 h-10 border border-white border-opacity-25 rounded-3xl text-white cursor-pointer lg:w-20 lg:h-20 lg:rounded-full lg:text-lg";
 
   const [largura, setLargura] = React.useState(0);
 
@@ -26,7 +26,7 @@ const Technology = () => {
   React.useEffect(() => {
     window.addEventListener("resize", atualizaImage);
 
-    if (largura >= 768) {
+    if (largura >= 768 && largura <= 1024) {
       setImage("landscape");
     } else setImage("portrait");
 
@@ -50,38 +50,43 @@ const Technology = () => {
   if (!data) return null;
   return (
     <div className="flex flex-col items-center h-full">
-      <h1 className="text-white tracking-wide pt-6 pb-20 md:self-start md:px-10 md:pt-10">
+      <h1 className="text-white tracking-wide pt-6 pb-20 md:self-start md:px-10 md:pt-10 lg:pl-32">
         <span className="font-bold opacity-25 pr-6 lg:text-nowrap">03</span>
         Lançamento espacial 101
       </h1>
 
-      <div className="w-full">
-        <img
-          className="h-72 w-screen"
-          src={`${data?.technology[indice].images[image]}`}
-        />
-      </div>
+      <div className="flex flex-col lg:flex-row-reverse lg:items-center lg:pl-32 lg:pb-16">
+        <div className="w-full">
+          <img
+            className="h-72 w-full lg:h-96"
+            src={`${data?.technology[indice].images[image]}`}
+          />
+        </div>
 
-      <ul className="flex gap-x-4 pt-8 pb-10" ref={elementoUl}>
-        <li className={estadoActive} onClick={atualizaComponentes}>
-          1
-        </li>
-        <li className={estadoOff} onClick={atualizaComponentes}>
-          2
-        </li>
-        <li className={estadoOff} onClick={atualizaComponentes}>
-          3
-        </li>
-      </ul>
+        <ul
+          className="flex gap-x-4 pt-8 pb-10 lg:flex-col lg:gap-y-4 lg:order-1 lg:pr-16"
+          ref={elementoUl}
+        >
+          <li className={estadoActive} onClick={atualizaComponentes}>
+            1
+          </li>
+          <li className={estadoOff} onClick={atualizaComponentes}>
+            2
+          </li>
+          <li className={estadoOff} onClick={atualizaComponentes}>
+            3
+          </li>
+        </ul>
 
-      <div className="flex flex-col items-center ">
-        <h2 className="text-2xl text-blue-secondary  ">A terminologia</h2>
-        <h3 className="text-4xl text-white ">
-          {data.technology[indice]?.name}
-        </h3>
-        <p className="text-center text-blue-secondary leading-6 pt-4 px-6 pb-12 md:px-32">
-          {data.technology[indice]?.description}
-        </p>
+        <div className="flex flex-col items-center lg:items-start lg:w-11/12">
+          <h2 className="text-2xl text-blue-secondary  ">A terminologia</h2>
+          <h3 className="text-4xl text-white ">
+            {data.technology[indice]?.name}
+          </h3>
+          <p className="text-center text-blue-secondary leading-6 pt-4 px-6 pb-12 md:px-32 lg:p-0 lg:text-start lg:pt-6 lg:pr-8">
+            {data.technology[indice]?.description}
+          </p>
+        </div>
       </div>
     </div>
   );
